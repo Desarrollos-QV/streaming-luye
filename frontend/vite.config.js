14a3fe2be32d7fce887f0329687b6750a11eed6e
Desarrollos-QV <soporte.desarrollosqv@gmail.com>
2026-09-03 21:50:12 -0600

    fix: Remover plugin vite de tailwind para evitar error de compilacion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
